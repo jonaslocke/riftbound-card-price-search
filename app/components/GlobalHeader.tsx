3205ab12cae3c62ffd6e9367417c3a27494586f8
Jonas Antunes <jonaslocke@gmail.com>
2025-12-17 14:42:34 -0300

    global header and search box

--- app/components/GlobalHeader.tsx
+++ app/components/GlobalHeader.tsx
@@ -2,22 +2,29 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import SearchForm from "./SearchForm";
 
 export default function GlobalHeader() {
   const pathname = usePathname();
   if (pathname === "/") return null;
 
   return (
-    <header className="global-header">
-      <div className="global-header__inner">
-        <Link className="global-brand" href="/">
-          <span className="global-brand__dot" aria-hidden="true" />
-          <span className="global-brand__text">Rift Search</span>
-        </Link>
-        <Link className="global-header__link" href="/">
-          Back to search
-        </Link>
-      </div>
-    </header>
+    <>
+      <header className="global-header">
+        <div className="global-header__inner">
+          <Link className="global-brand" href="/">
+            <span className="global-brand__dot" aria-hidden="true" />
+            <span className="global-brand__text">Rift Search</span>
+          </Link>
+          <div className="global-header__search">
+            <SearchForm placeholder="Search cards" />
+          </div>
+          <button className="global-avatar" type="button" aria-label="Account">
+            RS
+          </button>
+        </div>
+      </header>
+      <div className="global-header-spacer" aria-hidden="true" />
+    </>
   );
 }
