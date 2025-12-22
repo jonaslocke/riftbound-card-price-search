3de87211a3066891a03e81cd1267e527943b65db
Jonas Antunes <jonaslocke@gmail.com>
2025-12-22 12:52:12 -0300

    refactor card image

--- app/components/CardDetails.tsx
+++ app/components/CardDetails.tsx
@@ -1,10 +1,7 @@
-"use client";
-
-import { motion, useMotionValue, useSpring } from "motion/react";
 import { toCardDisplayData } from "@/lib/card-display-dto";
-import type { Card, CardDomain } from "../types/card";
 import { cn } from "@/lib/utils";
-import CardCost from "./CardCost";
+import type { Card, CardDomain } from "../types/card";
+import CardImage from "./CardImage";
 import CardTitle from "./CardTitle";
 
 type DomainBorderClass = `border-t-${CardDomain} border-b-${CardDomain}`;
@@ -20,48 +17,11 @@ const domainBorderColors: Record<CardDomain, DomainBorderClass> = {
 
 export default function CardDetails(card: Card) {
   const cardDetails = toCardDisplayData(card);
-  const { imageUrl, name, domains } = cardDetails;
-  const tiltX = useMotionValue(0);
-  const tiltY = useMotionValue(0);
-  const smoothTiltX = useSpring(tiltX, { stiffness: 180, damping: 18 });
-  const smoothTiltY = useSpring(tiltY, { stiffness: 180, damping: 18 });
-
-  const handleMouseMove = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    const rect = event.currentTarget.getBoundingClientRect();
-    const x = (event.clientX - rect.left) / rect.width;
-    const y = (event.clientY - rect.top) / rect.height;
-    const maxRotate = 6;
-    tiltY.set((x - 0.5) * maxRotate * 2);
-    tiltX.set((0.5 - y) * maxRotate * 2);
-  };
-
-  const handleMouseLeave = () => {
-    tiltX.set(0);
-    tiltY.set(0);
-  };
+  const { domains } = cardDetails;
 
   return (
     <div className="flex">
-      <div
-        className="z-1 w-[320]"
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-        style={{ perspective: "900px" }}
-      >
-        <motion.img
-          src={imageUrl}
-          alt={name}
-          className="w-full"
-          style={{
-            rotateX: smoothTiltX,
-            rotateY: smoothTiltY,
-            transformStyle: "preserve-3d",
-            filter: "drop-shadow(0 18px 30px rgba(0, 0, 0, 0.35))",
-          }}
-        />
-      </div>
+      <CardImage {...cardDetails} />
       <div
         className={cn(
           "bg-white/75 w-[356] h-[470] text-black -translate-x-5 translate-y-5",
---
+++ app/components/CardImage.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { CardDisplayData } from "@/lib/card-display-dto";
+import { motion, useMotionValue, useSpring } from "motion/react";
+
+export default function CardImage({ imageUrl, name }: CardDisplayData) {
+  const tiltX = useMotionValue(0);
+  const tiltY = useMotionValue(0);
+  const smoothTiltX = useSpring(tiltX, { stiffness: 180, damping: 18 });
+  const smoothTiltY = useSpring(tiltY, { stiffness: 180, damping: 18 });
+
+  const handleMouseMove = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const x = (event.clientX - rect.left) / rect.width;
+    const y = (event.clientY - rect.top) / rect.height;
+    const maxRotate = 6;
+    tiltY.set((x - 0.5) * maxRotate * 2);
+    tiltX.set((0.5 - y) * maxRotate * 2);
+  };
+
+  const handleMouseLeave = () => {
+    tiltX.set(0);
+    tiltY.set(0);
+  };
+  return (
+    <div
+      className="z-1 w-[320]"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ perspective: "900px" }}
+    >
+      <motion.img
+        src={imageUrl}
+        alt={name}
+        className="w-full"
+        style={{
+          rotateX: smoothTiltX,
+          rotateY: smoothTiltY,
+          transformStyle: "preserve-3d",
+          filter: "drop-shadow(0 18px 30px rgba(0, 0, 0, 0.35))",
+        }}
+      />
+    </div>
+  );
+}
