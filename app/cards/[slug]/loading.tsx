c4f78564731a12f878efad5b5f262e8351353b77
Jonas Antunes <jonaslocke@gmail.com>
2025-12-27 07:46:18 -0300

    loading icon

--- app/cards/[slug]/loading.tsx
+++ app/cards/[slug]/loading.tsx
@@ -1,11 +1,10 @@
+import loading from "@/assets/brand/loading.svg";
+import Image from "next/image";
+
 export default function Loading() {
   return (
-    <main className="flex flex-col w-full max-w-2xl">
-      <div className="mt-6 space-y-4 animate-pulse">
-        <div className="h-20 rounded-md bg-black/10" />
-        <div className="h-72 rounded-md bg-black/10" />
-        <div className="h-80 rounded-md bg-black/10" />
-      </div>
+    <main className="flex flex-col w-screen h-screen items-center justify-center">
+      <Image src={loading} alt="loading" className="w-1/3" />
     </main>
   );
 }
