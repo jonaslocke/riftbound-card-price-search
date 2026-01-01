635e80bde9315df330f9a04e9e6d6a782bcc00f1
Jonas Antunes <jonaslocke@gmail.com>
2026-01-01 16:16:07 -0300

    card_price mongodb collection

---
+++ lib/mongodb/collections.ts
@@ -0,0 +1,22 @@
+import type { Collection } from "mongodb";
+import { getDb } from "@/lib/mongodb";
+import type { CardPricesResponse } from "@/lib/prices/schema";
+
+export interface CardPriceDoc extends CardPricesResponse {
+  cachedAt: Date;
+}
+
+const COLLECTIONS = {
+  cardPrices: "card_price_cache",
+} as const;
+
+type Collections = {
+  cardPrices: Collection<CardPriceDoc>;
+};
+
+export async function getCollections(): Promise<Collections> {
+  const db = await getDb();
+  return {
+    cardPrices: db.collection<CardPriceDoc>(COLLECTIONS.cardPrices),
+  };
+}
