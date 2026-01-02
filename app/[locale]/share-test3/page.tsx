aef39d349719b5f10261cfbbbeea3cd3b3136074
Jonas Antunes <jonaslocke@gmail.com>
2026-01-02 16:13:53 -0300

    point to images

--- app/[locale]/share-test3/page.tsx
+++ app/[locale]/share-test3/page.tsx
@@ -15,8 +15,9 @@ export const metadata: Metadata = {
     url: "https://a.co/d/eAcVivS",
     images: [
       {
-        url: "https://hextechindex.com/81Z2YCqqy-L._AC_SL1500_.jpg",
-        secureUrl: "https://hextechindex.com/81Z2YCqqy-L._AC_SL1500_.jpg",
+        url: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/ad69bde670ce218adee1d2a618a7295d2fb7bd4c-744x1039.png",
+        secureUrl:
+          "https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/ad69bde670ce218adee1d2a618a7295d2fb7bd4c-744x1039.png",
         width: 1500,
         height: 1500,
         alt: "Kindle 16 GB (Geração mais recente) - Cor Preta",
@@ -30,7 +31,7 @@ export const metadata: Metadata = {
     description:
       "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
     images: [
-      "https://hextechindex.com/81Z2YCqqy-L._AC_SL1500_.jpg",
+      "https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/ad69bde670ce218adee1d2a618a7295d2fb7bd4c-744x1039.png",
     ],
   },
 };
