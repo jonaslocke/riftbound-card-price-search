2ed8029b01e9e6e235b49f0e4766bf24ec47b643
Jonas Antunes <jonaslocke@gmail.com>
2026-01-02 16:04:56 -0300

    meta share test

---
+++ amazon-share-html.md
@@ -0,0 +1,14 @@
+<meta property="og:type" content="product" />
+<meta property="og:site_name" content="Amazon.com.br" />
+<meta property="og:title" content="Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta" />
+<meta property="og:description" content="Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta" />
+<meta property="og:url" content="https://a.co/d/eAcVivS" />
+<meta property="og:image" content="https://m.media-amazon.com/images/I/81Z2YCqqy-L._AC_SL1500_.jpg" />
+<meta property="og:image:secure_url" content="https://m.media-amazon.com/images/I/81Z2YCqqy-L._AC_SL1500_.jpg" />
+<meta property="og:image:width" content="1500" />
+<meta property="og:image:height" content="1500" />
+<meta property="og:image:alt" content="Kindle 16 GB (Geração mais recente) - Cor Preta" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta" />
+<meta name="twitter:description" content="Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta" />
+<meta name="twitter:image" content="https://m.media-amazon.com/images/I/81Z2YCqqy-L._AC_SL1500_.jpg" />
---
+++ app/[locale]/share-test/page.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title:
+    "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
+  description:
+    "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
+  openGraph: {
+    type: "website",
+    siteName: "Amazon.com.br",
+    title:
+      "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
+    description:
+      "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
+    url: "https://a.co/d/eAcVivS",
+    images: [
+      {
+        url: "https://m.media-amazon.com/images/I/81Z2YCqqy-L._AC_SL1500_.jpg",
+        secureUrl:
+          "https://m.media-amazon.com/images/I/81Z2YCqqy-L._AC_SL1500_.jpg",
+        width: 1500,
+        height: 1500,
+        alt: "Kindle 16 GB (Geração mais recente) - Cor Preta",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title:
+      "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
+    description:
+      "Kindle 16 GB (Geração mais recente) - Leve e compacto, com tela antirreflexo, trocas de página mais rápidas, luz frontal ajustável e bateria de longa duração - Cor Preta",
+    images: [
+      "https://m.media-amazon.com/images/I/81Z2YCqqy-L._AC_SL1500_.jpg",
+    ],
+  },
+};
+
+export default async function ShareTest() {
+  return <h1 className="mt-36 text-white">share test</h1>;
+}
