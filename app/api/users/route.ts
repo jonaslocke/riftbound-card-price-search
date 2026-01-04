ceb053e8c425da32ba4006a62048f3ad828a16d9
Jonas Antunes <jonaslocke@gmail.com>
2026-01-04 17:58:30 -0300

    persist original users list

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,5 +1,23 @@
-import { getCollections } from "@/lib/mongodb/collections";
 import { NextRequest, NextResponse } from "next/server";
+
+const ORIGNAL_USERS = [
+  "andrei2804@gmail.com",
+  "bsantosanisio@gmail.com",
+  "caiov.borges32@gmail.com",
+  "calheira15@gmail.com",
+  "daniel.mota@globant.com",
+  "fteive@gmail.com",
+  "genilton.jr@gmail.com",
+  "gugagaviao@gmail.com",
+  "henriquegabriel.boschetti@gmail.com",
+  "jonaslocke@gmail.com",
+  "lucasvaz.ssm@gmail.com",
+  "mendesss.breno@gmail.com",
+  "pedroluizfracassi@gmail.com",
+  "ramonsousadossantos@gmail.com",
+  "tenazlima@gmail.com",
+  "xxsoaferxx@gmail.com",
+];
 
 export async function GET(req: NextRequest) {
   return NextResponse.json({ message: "I'm Alive" });
